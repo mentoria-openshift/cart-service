698af7da39691f13118da1be1a88c1f36f00ecaa
Thales Alves <thales@thalesalv.es>
2020-11-11 00:54:11 -0300

    criado gateway

---
+++ gateway/src/test/java/es/thalesalv/sigla/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package es.thalesalv.sigla.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
