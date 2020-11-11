3844d3e1faad64eb2ca39ed0fe0bae27446a65aa
Thales Alves <thales@thalesalv.es>
2020-11-11 00:54:00 -0300

    criado eureka

---
+++ eureka-server/src/test/java/es/thalesalv/sigla/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.thalesalv.sigla.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
