06d451848d5244f80fa05a6fbd7144bfe45032f4
Thales Alves <thales@thalesalv.es>
2020-11-16 22:50:13 -0300

    removido thymeleaf

--- gateway/src/main/java/es/thalesalv/sigla/gateway/adapter/controller/GreetingController.java
+++ gateway/src/main/java/es/thalesalv/sigla/gateway/adapter/controller/GreetingController.java
@@ -1,23 +1,34 @@
 package es.thalesalv.sigla.gateway.adapter.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@AuthenticationPrincipal OidcUser oidcUser, Model model,
+    @GetMapping("/auth/data")
+    public Map<String, Map<String, String>> authData(@AuthenticationPrincipal OidcUser oidcUser,
             @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client) {
-        
-        model.addAttribute("username", oidcUser.getEmail());
-        model.addAttribute("idToken", oidcUser.getIdToken());
-        model.addAttribute("accessToken", client.getAccessToken());
-        return "greeting";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", oidcUser.getPreferredUsername());
+        userData.put("email", oidcUser.getEmail());
+        userData.put("fullName", oidcUser.getFullName());
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("idToken", oidcUser.getIdToken().getTokenValue());
+        tokens.put("accessToken", client.getAccessToken().getTokenValue());
+
+        Map<String, Map<String, String>> authData = new HashMap<>();
+        authData.put("userData", userData);
+        authData.put("tokens", tokens);
+        return authData;
     }
 }
